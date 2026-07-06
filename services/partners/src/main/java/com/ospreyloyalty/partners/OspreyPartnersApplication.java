b42723807bbe4acf5ab926833b95b90b74126414
Daniel Klingstedt <daniel.klingstedt@gmail.com>
2026-07-06 17:11:18 +0200

    chore(partners): Spring Boot scaffold on Java 21 with maven wrapper

---
+++ services/partners/src/main/java/com/ospreyloyalty/partners/OspreyPartnersApplication.java
@@ -0,0 +1,13 @@
+package com.ospreyloyalty.partners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OspreyPartnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OspreyPartnersApplication.class, args);
+	}
+
+}
